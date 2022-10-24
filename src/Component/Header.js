a460d9c1363a406b4563909c36c3f989e0c7a081
Funsho <fejjy1998@gmail.com>
2022-10-24 20:32:12 +0100

    my fist commit on frontend stage 1 task - Zuri

---
+++ src/Component/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import img1 from '../profile__img.png';
+
+function Header() {
+  return (
+    <main>
+        <img src={img1} alt=""/>
+        <h2>James Funsho</h2>
+
+    </main>
+  )
+}
+
+export default Header
