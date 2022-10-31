3af754eff27de60c0c014baf5c5473c8be0bcc8b
Funsho <fejjy1998@gmail.com>
2022-10-31 18:09:49 +0100

    Edited codes for resubmission

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import img1 from '../profile__img.png';
 import logo from '../Button.jpg';
+import icon2 from '../Share.jpg';
 
 
 // My profile section that covers the profile image and twitter id
@@ -10,11 +11,14 @@ function Header() {
       <div className='image'>
         <img src={logo} alt=""/>
       </div>
+      <div className='shareButton'><img src={icon2} alt=""/></div>
 
       <div className='Profile'>
-        <img src={img1} alt=""/>
-        <h2>Jamesfunsho3</h2>
+        <img src={img1} alt="" id='profile__img'/>
+        <h2>James Funsho</h2>
       </div>
+
+      <div className='slackName' id='slack'><p>Fej</p></div>
 
     </main>
   )
--- src/Component/Link.js
+++ src/Component/Link.js
@@ -4,11 +4,12 @@ import React from 'react'
 function Link() {
   return (
     <section className='Link-section'>
-        <a href='https://training.zuri.team' target="_blank" rel="noreferrer"><button>btn__zuri</button></a>
-        <a href='http://books.zuri.team' target="_blank" rel="noreferrer"><button>books</button></a>
-        <a href='https://books.zuri.team/' target="_blank" rel="noreferrer"><button>book__python</button></a>
-        <a href='https://background.zuri.team' target="_blank" rel="noreferrer"><button>pitch</button></a>
-        <a href='https://books.zuri.team/design-rules' target="_blank" rel="noreferrer"><button>book__design</button></a>
+        <a href='http://twitter/jamesfunsho3' target="_blank" rel="noreferrer" id='twitter'><button>Twitter Link</button></a>
+        <a href='https://training.zuri.team' target="_blank" rel="noreferrer" id='btn__zuri'><button>Zuri Team</button></a>
+        <a href='http://books.zuri.team' target="_blank" rel="noreferrer" id='books'><button>Zuri Books</button></a>
+        <a href='https://books.zuri.team/python-for-beginners?ref_id=<Fej>' target="_blank" rel="noreferrer" id='book__python'><button>Python Books</button></a>
+        <a href='https://background.zuri.team' target="_blank" rel="noreferrer" id='pitch'><button>Background Checks for Coders</button></a>
+        <a href='https://books.zuri.team/design-rules' target="_blank" rel="noreferrer" id='book__design'><button>Design Books</button></a>
     </section>
   )
 }
