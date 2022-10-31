ba9b545e32f47dc2b0e056a9ccfc8b034389ae8e
Funsho <fejjy1998@gmail.com>
2022-10-31 18:43:16 +0100

    twitter link commit

--- src/Component/Link.js
+++ src/Component/Link.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Link() {
   return (
     <section className='Link-section'>
-        <a href='http://twitter/jamesfunsho3' target="_blank" rel="noreferrer" id='twitter'><button>Twitter Link</button></a>
+        <a href='http://twitter.com/jamesfunsho3' target="_blank" rel="noreferrer" id='twitter'><button>Twitter Link</button></a>
         <a href='https://training.zuri.team' target="_blank" rel="noreferrer" id='btn__zuri'><button>Zuri Team</button></a>
         <a href='http://books.zuri.team' target="_blank" rel="noreferrer" id='books'><button>Zuri Books</button></a>
         <a href='https://books.zuri.team/python-for-beginners?ref_id=<Fej>' target="_blank" rel="noreferrer" id='book__python'><button>Python Books</button></a>
